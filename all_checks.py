2d8e42e75241ffa5efc50e0e03f9d904ac020b72
Soyoung Chung <soyoung823@gmail.com>
2025-07-12 13:52:59 -0700

    Renamed min_absolute to min_gb and used parameter names

--- all_checks.py
+++ all_checks.py
@@ -6,11 +6,11 @@ import sys
 def check_reboot():
     return os.path.exists("/run/reboot-required")
 
-def check_disk_full(disk, min_absolute, min_percent):
+def check_disk_full(disk, min_gb, min_percent):
     du = shutil.disk_usage(disk)
     percent_free = 100 * du.free / du.total
     gigabytes_free = du.free / 2**30
-    if percent_free < min_percent or gigabytes_free < min_absolute:
+    if percent_free < min_percent or gigabytes_free < min_gb:
         return False
     return True
 
@@ -18,7 +18,7 @@ def main():
     if not check_reboot():
         print("Pending Reboot")
         sys.exit(1)
-    if check_disk_full("/", 2, 10):
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
         print("Disk full.")
         sys.exit(1)
 
