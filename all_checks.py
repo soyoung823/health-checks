b7ec65c1162f5b39b8300a6145a48ab6785af2fe
Soyoung Chung <soyoung823@gmail.com>
2025-07-12 14:40:15 -0700

    Iterated over a list of checks and messages to avoid code repetition

--- all_checks.py
+++ all_checks.py
@@ -20,19 +20,15 @@ def check_root_full():
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
 def main():
-    #checks = [
-    #        (check_reboot, "Pendig Reboot."),
-    #        (check_root_full, "Root partition full"),
-    #        ]
-    if check_reboot():
-        print("Pending Reboot")
-        sys.exit(1)
-    if check_root_full():
-        print("Root partition full.")
-        sys.exit(1)
-    #if check_disk_full(disk="/", min_gb=2, min_percent=10):
-    #    print("Disk full.")
-    #    sys.exit(1)
+    checks = [
+            (check_reboot, "Pendig Reboot."),
+            (check_root_full, "Root partition full"),
+            ]
+    for check, msg in checks:
+        if check():
+            print(msg)
+            sys.exit(1)
+    
     print("Everything ok.")
     sys.exit(0)
 
